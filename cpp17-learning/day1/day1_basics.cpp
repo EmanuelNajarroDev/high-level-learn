121b4db67e0f69ec5ce9592f022828c0e16cdb5a
Emanuel Najarro <emanuel.najarro.dev@gmail.com>
2025-05-28 18:54:25 -0400

    Created day1 directory within cpp17-learning and created the day1_basics file that was moved into day1

---
+++ cpp17-learning/day1/day1_basics.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+string name;
+
+cout << "Hello! What is your name?" << endl;
+
+cin >> name;
+
+cout << "Hey " << name << "!";
+
+
+for(int i = 0; i <= 100; i+=2){
+
+    cout << i << endl;
+
+}
+
+
+
+}
