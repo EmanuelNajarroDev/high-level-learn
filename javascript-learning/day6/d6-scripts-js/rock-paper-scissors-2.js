bdf3631fc2f7f606b04bdcb6209cfc4cf64b2924
Emanuel Najarro <emanuel.najarro.dev@gmail.com>
2025-06-27 15:44:49 -0400

    Created rock-paper-scissor-2. Implemented css in rock-paper-scissors-2.css to style the html, andand keep it organized. Rps-.js contains the javascript previously within the html. Added icons and created new folders to hold the css, js, and images

---
+++ javascript-learning/day6/d6-scripts-js/rock-paper-scissors-2.js
@@ -0,0 +1,103 @@
+const score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
+  
+document.querySelector('.js-score')
+  .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+
+  //Converted score object into a
+//JSON string to be parsed within score, it is retrieved from local storage as a JSON and then converted
+//back into an object 
+
+/*
+if(!score){//shortcut for if score === null
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
+}
+*/
+
+function playGame(playerMove){
+
+  const computerMove = pickComputerMove();
+
+  let result = '';
+
+  if(playerMove === 'rock'){
+    if(computerMove === 'rock'){
+      result = 'Tie';
+    }else if(computerMove === 'paper'){
+      result = 'You lose';
+    }else if(computerMove === 'scissors'){
+      result = 'You win';
+    }
+  }
+  
+  if(playerMove === 'paper'){
+    if(computerMove === 'rock'){
+      result = 'You win';
+    }else if(computerMove === 'paper'){
+      result = 'Tie';
+    }else if(computerMove === 'scissors'){
+      result = 'You lose';
+    }
+  }
+
+  if(playerMove === 'scissors'){
+    if(computerMove === 'rock'){
+      result = 'You lose';
+    }else if(computerMove === 'paper'){
+      result = 'You win';
+    }else if(computerMove === 'scissors'){
+      result = 'Tie';
+    }
+  }
+  
+  if (result === 'You win'){
+    score.wins += 1;
+  } else if (result === 'You lose'){
+    score.losses += 1;
+  } else if (result === 'Tie'){
+    score.ties += 1;
+  }
+
+  localStorage.setItem('score',JSON.stringify(score));//saves our score into local storage so it is not deleted if the page refreshes
+  //Also turns our score into a string because JSON only accepts strings
+
+  updateScoreElement();
+
+  document.querySelector('.js-result').
+    innerHTML = result;
+
+  document.querySelector('.js-moves').innerHTML
+  = `You 
+<img src="images/${playerMove}-emoji.png" class="move-icon">
+<img src="images/${computerMove}-emoji.png" class="move-icon">
+Computer` 
+
+}    
+
+function updateScoreElement(){
+  document.querySelector('.js-score')
+    .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove(){
+  const randomNumber = Math.random();
+
+  let computerMove = '';
+
+  if (randomNumber >= 0 && randomNumber < 1/3)
+  {
+    computerMove = 'rock';
+  }else if(randomNumber >= 1/3 && randomNumber < 2/3){
+    computerMove = 'paper';
+  }else if(randomNumber >= 2/3 && randomNumber < 1){
+    computerMove = 'scissors';
+  }
+  return computerMove;
+}
